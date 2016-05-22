ec274a15ab3efbdde02da48e93c242ca5231e610
Asymons <Asymons@users.noreply.github.com>
2016-05-22 00:27:31 -0400

    Update Arduino-code.ino

--- Arduino-code.ino
+++ Arduino-code.ino
@@ -3,8 +3,14 @@ void setup() {
   // initialize serial communication:
   Serial.begin(9600);
   // initialize the LED pins:
-  for (int thisPin = 2; thisPin < 7; thisPin++) {
+  for (int thisPin = 8; thisPin < 13; thisPin++) {
     pinMode(thisPin, OUTPUT);
+  }
+}
+
+void pinLight(int x){
+  for (int thisPin = 8; thisPin < 8+x; thisPin++) {
+      digitalWrite(thisPin, HIGH);
   }
 }
 
@@ -20,23 +26,23 @@ void loop() {
 
     switch (inByte) {
       case 'a':
-        digitalWrite(8, HIGH);
+        pinLight(1);
         break;
       case 'b':
-        digitalWrite(9, HIGH);
+        pinLight(2);
         break;
       case 'c':
-        digitalWrite(10, HIGH);
+        pinLight(3);
         break;
       case 'd':
-        digitalWrite(11, HIGH);
+        pinLight(4);
         break;
       case 'e':
-        digitalWrite(12, HIGH);
+        pinLight(5);
         break;
       default:
         // turn all the LEDs off:
-        for (int thisPin = 8; thisPin < 12; thisPin++) {
+        for (int thisPin = 8; thisPin < 13; thisPin++) {
           digitalWrite(thisPin, LOW);
         }
     }
